aadf127bf4fce0af25d93a34f8c0fa9abffd19dd
Nyrrell <morel.bapt@gmail.com>
2021-09-23 17:57:09 +0200

    Add StickerCard component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,5 +24,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   height: auto;
+  background: hsl(0, 0%, 96%);
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home-img"></div>
+    <div class="container is-flex is-flex-wrap-wrap is-justify-content-space-around mt-6">
+      <StickerCard v-for="i of 16" :key="i" :name="i"></StickerCard>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StickerCard from '@/components/StickerCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    StickerCard
   }
 }
 </script>
+
+<style>
+.home-img {
+  height: 400px;
+  background-size: cover;
+  background: url("https://images.unsplash.com/photo-1540651810471-5699df74834f") no-repeat local, fixed center;
+  border-bottom: 4px groove hsl(204, 86%, 53%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
