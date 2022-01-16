f576884866b1be553b4f8d721f6774aa62cb1b72
nyrrell <morel.bapt@gmail.com>
2022-01-16 17:55:28 +0100

    change stats

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,6 +1,6 @@
 import Order from "../models/Order.js";
 
-async function orderRoute(fastify, options) {
+async function orderRoute(fastify) {
 
   // CREATE ORDER
   fastify.post('/',
@@ -90,13 +90,21 @@ async function orderRoute(fastify, options) {
     async (req, res) => {
       if (!req.user.isAdmin) return res.code(403).send('Unauthorized');
 
+      const productId = req.query.pid;
       const date = new Date();
       const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
       const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
       try {
         const income = await Order.aggregate([
-          { $match: { createdAt: { $gte: previousMonth } } },
+          {
+            $match: {
+              createdAt: { $gte: previousMonth },
+              ...(productId && {
+                products: { $elemMatch: { productId } },
+              }),
+            },
+          },
           {
             $project: {
               month: { $month: "$createdAt" },
